9c4ae398b8655f66c91b960742bb376c0e659fc2
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-08-01 14:23:51 +0800

    add transition to begin button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+
+	$('.start__button').click(function () {
+		$('.start').fadeOut(function () {
+			$('.li-form form').fadeIn()
+		})
+	})
+
+
 	$('.form-field__button').click(function (e) {
 		if (e.target.dataset.gender) {
 			if (e.target.dataset.gender == 'f') {
@@ -10,4 +18,6 @@ $(document).ready(function () {
 			}
 		}
 	})
+
+
 })
