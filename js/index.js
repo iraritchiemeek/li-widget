89d7b36a6177daae975b3e9e18c3a1635cc57229
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-08-01 15:18:01 +0800

    add generalized fn for select animation and add fn for action button

--- js/index.js
+++ js/index.js
@@ -3,21 +3,45 @@ $(document).ready(function () {
 	$('.start__button').click(function () {
 		$('.start').fadeOut(function () {
 			$('.li-form form').fadeIn()
+			setCurrentFieldData('gender')
+			showCurrentFormField()
 		})
 	})
 
-
-	$('.form-field__button').click(function (e) {
-		if (e.target.dataset.gender) {
-			if (e.target.dataset.gender == 'f') {
-				$('.form-field__selected').animate({'right': '10px'})
-				$('.form-field__selected').css({'left': 'initial'})
-			} else {
-				$('.form-field__selected').animate({'left': '10px'})
-				$('.form-field__selected').css({'right': 'initial'})
-			}
+	$('.li-form__action').click(function () {
+		if ($('.li-form form').data('current') == 'gender') {
+			$('.li-form form').fadeIn(function () {
+				setCurrentFieldData('smoker')
+				showCurrentFormField()
+			})
 		}
 	})
 
+	$('.form-field__button').click(function(e) {
+		if (e.target.dataset.pos == 'right') {
+			$('.form-field__selected').animate({'right': '10px'})
+			$('.form-field__selected').css({'left': 'initial'})
+		} else {
+			$('.form-field__selected').animate({'left': '10px'})
+			$('.form-field__selected').css({'right': 'initial'})
+		}
+	})
+
+	function setCurrentFieldData(fieldName) {
+		$('.li-form form').data('current', fieldName)
+	}
+
+	function showCurrentFormField() {
+		switch($('.li-form form').data('current')) {
+		    case 'gender':
+		    	$('.form-field').css({'display': 'none'})
+		        $('.form-field--gender').show()
+		        break;
+		    case 'smoker':
+		    	$('.form-field').css({'display': 'none'})
+				$('.form-field--smoker').fadeIn()
+		    	break;
+		}
+	}
 
 })
