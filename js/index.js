ccf8636691640c60b1906e78f939cf3331dbe17f
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-08-02 09:10:17 +0800

    style age page with check for valid age

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
 	$('.start__button').click(function () {
 		$('.start').fadeOut(function () {
-			$('.li-form form').fadeIn()
 			setCurrentFieldData('gender')
 			showCurrentFormField()
 		})
@@ -10,19 +9,24 @@ $(document).ready(function () {
 
 	$('.li-form__action').click(function () {
 		if ($('.li-form form').data('current') == 'gender') {
-			$('.li-form form').fadeIn(function () {
-				setCurrentFieldData('smoker')
-				showCurrentFormField()
-			})
+			setCurrentFieldData('smoker')
+			showCurrentFormField()
+		} else if ($('.li-form form').data('current') == 'smoker') {
+			setCurrentFieldData('age')
+			showCurrentFormField()
+		} else if ($('.li-form form').data('current') == 'age') {
+			// setCurrentFieldData('cover-amount')
+			// showCurrentFormField()
+			validAge()
 		}
 	})
 
 	$('.form-field__button').click(function(e) {
 		if (e.target.dataset.pos == 'right') {
-			$('.form-field__selected').animate({'right': '10px'})
+			$('.form-field__selected').animate({'right': '20px'})
 			$('.form-field__selected').css({'left': 'initial'})
 		} else {
-			$('.form-field__selected').animate({'left': '10px'})
+			$('.form-field__selected').animate({'left': '20px'})
 			$('.form-field__selected').css({'right': 'initial'})
 		}
 	})
@@ -34,13 +38,36 @@ $(document).ready(function () {
 	function showCurrentFormField() {
 		switch($('.li-form form').data('current')) {
 		    case 'gender':
-		    	$('.form-field').css({'display': 'none'})
+		    	$('.form-field').hide()
 		        $('.form-field--gender').show()
+				$('.li-form form').fadeIn()
 		        break;
 		    case 'smoker':
-		    	$('.form-field').css({'display': 'none'})
-				$('.form-field--smoker').fadeIn()
+		    	$('.li-form form').fadeOut(function () {
+					$('.form-field--gender').hide()
+					$('.form-field--smoker').show()
+					$('.li-form form').fadeIn()
+		    	})
 		    	break;
+	        case 'age':
+	        	$('.li-form form').fadeOut(function () {
+	    			$('.form-field--smoker').hide()
+	    			$('.form-field--age').show()
+	    			$('.li-form form').fadeIn(function () {
+	    				$('input').focus()
+	    			})
+	        	})
+	        	break;
+		}
+	}
+
+	function validAge() {
+		var age = $('input').val()
+		if (age < 20 || age >= 70 ) {
+			$('.form-field--age span').css({'opacity': 1})
+			return false
+		} else {
+			return true
 		}
 	}
 
